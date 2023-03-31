7f6b9d73bf14be90a5c94a0899d5e26d91108d7c
Serdar Cakir <serdar@visiosoft.com.tr>
2023-03-31 14:37:30 +0300

    fixed error

--- src/Http/Controller/Admin/CategoryController.php
+++ src/Http/Controller/Admin/CategoryController.php
@@ -125,7 +125,6 @@ class CategoryController extends AdminController
                         'parent_category' => $all['parent_category'] === "" ? null : $all['parent_category'],
                         'seo_keyword' => $all['seo_keyword'],
                         'seo_description' => $all['seo_description'],
-                        'seo_title' => $all['seo_title']
                     ]));
 
                     $this->createIconFile($category->getId());
@@ -153,7 +152,6 @@ class CategoryController extends AdminController
 
                         continue;
                     }
-
                     $category = $this->categoryRepository->create(array_merge($translatableEntries, [
                         'slug' => $slug,
                         'parent_category' => $all['parent_category'] === "" ? null : $all['parent_category'],
