bd621e8e8edee332b731b16438c5b64daee5b215
profstyle1 <46463523+profstyle1@users.noreply.github.com>
2023-08-25 16:43:07 +0300

    New translations message.php (Swedish)

--- resources/lang/sv/message.php
+++ resources/lang/sv/message.php
@@ -2,4 +2,8 @@
 
 return [
     'categories_mass_delete_success' => 'Kategorier och relaterade underkategorier har tagits bort!',
+    'cat_slug_exists' => 'A category with a slug of :slug already exists!',
+    'not_found' => ':name not found!',
+    'cat_no_sub' => 'Selected category has no sub-categories.',
+    'sub_limit' => 'You have reached your sub-category limit, you can only add 9 sub-categories.',
 ];
